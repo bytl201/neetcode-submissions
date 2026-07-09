90b02bfd191f4a3a89ac9dd17ef18f33e1257eb8
bytl201 <tlobden79@gmail.com>
2026-07-09 10:12:00 -0400

    Add: two-integer-sum - submission-33

---
+++ Data Structures & Algorithms/two-integer-sum/submission-33.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        count = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in count:
+                return [ count[target-nums[i]], i]
+            else:
+                print(i)
+                count[nums[i]] = i
