be411e435adae9bba8a2324ba61c0d540ed3067b
bytl201 <tlobden79@gmail.com>
2026-07-16 11:03:56 -0400

    Add: single-row-keyboard - submission-1

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        result = 0
+        last_char = keyboard[0]
+
+        for char in word:
+            time = abs(keyboard.index(last_char) - keyboard.index(char))
+            result += time
+            last_char = char
+        
+        return result
