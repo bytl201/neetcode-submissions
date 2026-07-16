fe281cb1a768a7f35753ec771660332d1ad633e7
bytl201 <tlobden79@gmail.com>
2026-07-15 21:51:10 -0400

    Add: largest-unique-number - submission-0

---
+++ Data Structures & Algorithms/largest-unique-number/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        count = {}
+
+        for i in nums:
+            count[i] = count.get(i, 0) + 1
+
+        result = [[] for i in range(len(count)+1)]
+
+        for n, o in count.items():
+            result[o].append(n)
+        
+        if result[1]:
+            return result[1][-1]
+        else:
+            return -1
