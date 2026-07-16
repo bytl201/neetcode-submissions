9ae167abc9c0f213c0f987efbe91ee546ef95c9c
bytl201 <tlobden79@gmail.com>
2026-07-16 11:50:54 -0400

    Add: single-row-keyboard - submission-4

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        maps = {}
+        for i,v in enumerate(keyboard):
+            maps[v] = i
+
+        result = 0
+        last_char = keyboard[0]
+
+        for char in range(len(word)):
+
+            time = abs(maps[last_char] - maps[word[char]])
+            result += time
+            last_char = word[char]
+        
+        return result
