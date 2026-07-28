9217bfceea717cef2e901c52a7f2d42ab61176c9
bytl201 <tlobden79@gmail.com>
2026-07-27 22:37:42 -0400

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        left, right = 0, 1
+        result = 0
+
+        while right < len(s):
+            result += abs(ord(s[right]) - ord(s[left]))
+            left += 1
+            right += 1
+
+        return result
