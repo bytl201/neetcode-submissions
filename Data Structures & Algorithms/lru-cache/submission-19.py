343edc9999708eb43de845f222e288b251e574e4
bytl201 <tlobden79@gmail.com>
2026-08-05 20:16:57 -0400

    Add: lru-cache - submission-19

---
+++ Data Structures & Algorithms/lru-cache/submission-19.py
@@ -0,0 +1,47 @@
+class ListNode():
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+        self.prev, self.next = None, None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.map = {}
+        self.left, self.right = ListNode(0,0), ListNode(0,0)
+
+        self.left.next, self.right.prev = self.right, self.left
+        
+    def insert(self, node):
+        prev_right, right = self.right.prev, self.right
+        prev_right.next, right.prev = node, node
+        node.prev, node.next = prev_right, right
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+
+    def get(self, key: int) -> int:
+        if key in self.map:
+            self.remove(self.map[key])
+            self.insert(self.map[key])
+            return self.map[key].value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            self.remove(self.map[key])
+
+        self.map[key] = ListNode(key, value)
+        self.insert(self.map[key])
+
+
+        if len(self.map) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            self.map.pop(lru.key, None)
+
+        
