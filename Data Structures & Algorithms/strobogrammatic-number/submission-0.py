f50af61f3a09c746d2c023a319f13f50aa891477
bytl201 <tlobden79@gmail.com>
2026-08-29 22:22:32 -0400

    Add: strobogrammatic-number - submission-0

---
+++ Data Structures & Algorithms/strobogrammatic-number/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        reverse = { '0':'0', '1':'1', '6':'9', '8':'8', '9':'6'}
+
+        left, right = 0, len(num) - 1
+
+        while left <= right:
+            if num[left] not in reverse or num[right] not in reverse:
+                return False
+
+            if reverse[num[left]] != num[right]:
+                return False
+
+            left+=1
+            right-=1
+
+        return True
